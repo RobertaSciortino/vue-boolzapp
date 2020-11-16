cd9528bcfd1ad0c6b6e00a0b2dd69427eb062f38
Roberta <robertasciortino.ag@gmail.com>
2020-11-16 12:30:27 +0100

    aggiunta icona  dropdown menu  su ogni messaggio

--- assets/main.js
+++ assets/main.js
@@ -12,17 +12,20 @@ var app = new Vue (
             {
               date: '10/01/2020 15:30:55',
               message: 'Hai portato a spasso il cane?',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Ricordati di dargli da mangiare',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '10/01/2020 16:15:22',
               message: 'Tutto fatto!',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             }
           ],
           contactClasses: [
@@ -40,17 +43,20 @@ var app = new Vue (
             {
               date: '20/03/2020 16:30:00',
               message: 'Ciao come stai?',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '20/03/2020 16:30:55',
               message: 'Bene grazie! Stasera ci vediamo?',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             },
             {
               date: '20/03/2020 16:35:00',
               message: 'Mi piacerebbe ma devo andare a fare la spesa',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             }
           ],
           contactClasses: [
@@ -68,17 +74,20 @@ var app = new Vue (
             {
               date: '28/03/2020 10:10:40',
               message: 'La Marianna va in campagna',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             },
             {
               date: '28/03/2020 10:20:10',
               message: 'Sicuro di non aver sbagliato chat?',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '28/03/2020 16:15:22',
               message: 'Ah scusa!',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             }
           ],
           contactClasses: [
@@ -96,12 +105,14 @@ var app = new Vue (
             {
               date: '10/01/2020 15:30:55',
               message: 'Lo sai che ha aperto una nuova pizzeria?',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Si, ma preferirei andare al cinema',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             }
           ],
           contactClasses: [
@@ -119,12 +130,14 @@ var app = new Vue (
             {
               date: '10/01/2020 15:30:55',
               message: 'Andiamo al cinema stasera?',
-              status: 'sent'
+              status: 'sent',
+              activeMenu: false
             },
             {
               date: '10/01/2020 15:50:00',
               message: 'Si, ci vediamo alle 20.00 :)',
-              status: 'received'
+              status: 'received',
+              activeMenu: false
             }
           ],
           contactClasses: [
@@ -143,7 +156,8 @@ var app = new Vue (
         message: 'Ciao!!',
         status: 'received'
       },
-      userSearch:''
+      userSearch:'',
+
 
     },
     methods: {
@@ -171,7 +185,13 @@ var app = new Vue (
           contact.visible = contact.name.toLowerCase().startsWith(this.userSearch.toLowerCase());
           console.log(contact.visible);
         });
+      },
+      showIconDropdownMenu(item) {
+        item.activeMenu = true;
 
+      },
+      hideIconDropdownMenu(item) {
+        item.activeMenu = false;
       }
 
     }
